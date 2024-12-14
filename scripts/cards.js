ee0ac360cbce8f666652df89bf3afd21f1af28a0
SamRosto <ryaguzov.m@gmail.com>
2024-12-15 00:19:05 +0200

    first attempt

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,45 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
+
+const placesSection = document.querySelector(".places");
+const placesContainer = placesSection.querySelector(".places__list");
+
+const createCard = (el) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = el.link;
+  cardElement.querySelector(".card__description").textContent = el.name;
+  return cardElement;
+};
+
+initialCards.forEach((el) => {
+  const cardElement = createCard(el);
+  placesContainer.append(cardElement);
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,88 @@
 // @todo: Темплейт карточки
+const placesSection = document.querySelector(".places");
+const placesContainer = placesSection.querySelector(".places__list");
+let placeListItems = placesContainer.querySelectorAll("li");
+const placeListItemsArray = Array.from(placeListItems);
+let deleteCardBtn = document.querySelector(".card__delete-button");
+let deleteCardBtnList = document.querySelectorAll(".card__delete-button");
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+const createCard = (el) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = el.link;
+  cardElement.querySelector(".card__description").textContent = el.name;
+  return cardElement;
+};
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const newCardBtn = document.querySelector(".profile__add-button");
+const popUpWindow = document.querySelector(".popup");
+const popUpForm = document.querySelector(".popup__form");
+
+const popUpNewCard = document.querySelector(".popup_type_new-card");
+const newCardCloseBtn = popUpNewCard.querySelector(".popup__close");
+const addPlaceBtn = popUpNewCard.querySelector(".popup__button");
+
+function allCards() {
+  const placeListItemsArray = placesContainer.querySelectorAll("li");
+
+  placeListItemsArray.forEach((el) => {
+    console.log(el);
+  });
+}
+
+const showPopUpNewCard = () => {
+  popUpNewCard.classList.add("popup_is-opened");
+};
+
+const closePopUpNewCard = () => {
+  popUpNewCard.classList.remove("popup_is-opened");
+};
+
+// newCardBtn.addEventListener("click", showPopUpNewCard);
+newCardBtn.addEventListener("click", showPopUpNewCard);
+newCardCloseBtn.addEventListener("click", closePopUpNewCard);
+
+function render() {
+  placeListItems = placesContainer.querySelectorAll("li");
+  placeListItemsArray.push(cardElement);
+}
+
+// Add new place
+addPlaceBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const placeName = document.querySelector(".popup__input_type_card-name");
+  const placeLink = document.querySelector(".popup__input_type_url");
+  console.log(placeName.value);
+
+  const cardObj = {
+    name: placeName.value,
+    link: placeLink.value,
+  };
+
+  const cardElement = createCard(cardObj);
+  placesContainer.append(cardElement);
+  closePopUpNewCard();
+  render();
+});
+
+deleteCardBtn.addEventListener("mouseover", function (e) {
+  const placeListItems = placesContainer.querySelectorAll("li");
+
+  placeListItems.forEach((el, index) => {
+    el.addEventListener("click", function (e) {
+      e.target.closest("li").remove();
+    });
+  });
+});
+
+// https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-sunset-in-the-background-7lzw_-bE7LA
